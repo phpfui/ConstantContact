59004115c9d4d07d00d9943d04e1233d5a6c0018
Bruce Wells <brucekwells@gmail.com>
2026-06-13 01:11:23 -0400

    V26.6.2 - 2026-06-13

---
+++ src/ConstantContact/V3/Billing/Branding/Logo.php
@@ -0,0 +1,13 @@
+<?php
+
+// Generated file. Do not edit by hand. Use update.php in project root.
+
+namespace PHPFUI\ConstantContact\V3\Billing\Branding;
+
+class Logo extends \PHPFUI\ConstantContact\Base
+	{
+	public function __construct(\PHPFUI\ConstantContact\Client $client)
+		{
+		parent::__construct($client, '/v3/billing/branding/logo');
+		}
+	}
