37d43fe1691ed3cc9b0161c22eaa98bae926e633
Bruce Wells <brucekwells@gmail.com>
2026-06-23 01:11:27 -0400

    V26.6.3 - 2026-06-23

---
+++ src/ConstantContact/V3/Billing/Nextpayment.php
@@ -0,0 +1,13 @@
+<?php
+
+// Generated file. Do not edit by hand. Use update.php in project root.
+
+namespace PHPFUI\ConstantContact\V3\Billing;
+
+class Nextpayment extends \PHPFUI\ConstantContact\Base
+	{
+	public function __construct(\PHPFUI\ConstantContact\Client $client)
+		{
+		parent::__construct($client, '/v3/billing/next-payment');
+		}
+	}
